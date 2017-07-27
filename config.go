d2e4a63eee33f70bb4216ff09107103d38e2f0e4
Preetam Jinka <pj@preet.am>
2017-07-27 00:07:47 -0400

    add group_columns to CloudWatch Log group config

--- config.go
+++ config.go
@@ -1,8 +1,9 @@
 package main
 
 type CloudWatchLogGroup struct {
-	Name    string `json:"name"`
-	FlowLog bool   `json:"flowlog"`
+	Name         string   `json:"name"`
+	FlowLog      bool     `json:"flowlog"`
+	GroupColumns []string `json:"group_columns,omitempty"`
 }
 
 type Config struct {
