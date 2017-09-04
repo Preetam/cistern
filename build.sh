285a87d658901a3c6ece7c048a0513d9952bd0b9
Preetam Jinka <pj@preet.am>
2017-09-04 09:50:32 -0400

    fix go builds

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 set -e
 cd ~/.go_workspace/src/github.com/Cistern/cistern
-go build ./cmd/cistern -o cistern-linux-amd64 && mv cistern-linux-amd64 $CIRCLE_ARTIFACTS
-GOOS=darwin GOARCH=amd64 go build ./cmd/cistern -o cistern-darwin-amd64 && mv cistern-darwin-amd64 $CIRCLE_ARTIFACTS
+go build -o cistern-linux-amd64 ./cmd/cistern && mv cistern-linux-amd64 $CIRCLE_ARTIFACTS
+GOOS=darwin GOARCH=amd64 go build -o cistern-darwin-amd64 ./cmd/cistern && mv cistern-darwin-amd64 $CIRCLE_ARTIFACTS
 cd ui
 npm i
 npm run build
