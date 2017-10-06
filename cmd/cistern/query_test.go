7c28f6500916fe78f001676327ef1282e34d630f
Preetam Jinka <pj@preet.am>
2017-10-06 00:29:53 -0400

    add new filter code

--- cmd/cistern/query_test.go
+++ cmd/cistern/query_test.go
@@ -29,3 +29,32 @@ func TestLimit(t *testing.T) {
 		t.Errorf("expected at most %d events but got %d", limit, len(result.Events))
 	}
 }
+
+func TestFilter(t *testing.T) {
+	ec, err := CreateEventCollection("/tmp/test_cistern_limit.lm2")
+	defer ec.col.Destroy()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = ec.StoreEvents(testEvents)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	result, err := ec.Query(query.Desc{
+		Filters: []query.Filter{
+			{
+				Column:    "source_port",
+				Condition: "=",
+				Value:     443.0,
+			},
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(result.Events) != 3 {
+		t.Errorf("expected %d events but got %d", 3, len(result.Events))
+	}
+}
