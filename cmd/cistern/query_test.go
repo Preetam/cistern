80194444c5187848ab746d96a29b389a71734609
Preetam Jinka <pj@preet.am>
2017-10-21 12:57:23 -0400

    add more filter tests

--- cmd/cistern/query_test.go
+++ cmd/cistern/query_test.go
@@ -31,7 +31,7 @@ func TestLimit(t *testing.T) {
 }
 
 func TestFilter(t *testing.T) {
-	ec, err := CreateEventCollection("/tmp/test_cistern_limit.lm2")
+	ec, err := CreateEventCollection("/tmp/test_cistern_filter.lm2")
 	defer ec.col.Destroy()
 	if err != nil {
 		t.Fatal(err)
@@ -41,20 +41,94 @@ func TestFilter(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	result, err := ec.Query(query.Desc{
-		Filters: []query.Filter{
-			{
-				Column:    "source_port",
-				Condition: "=",
-				Value:     443.0,
-			},
-		},
-	})
-	if err != nil {
-		t.Fatal(err)
+	type testCase struct {
+		Filters         []query.Filter
+		ExpectedMatches int
 	}
 
-	if len(result.Events) != 3 {
-		t.Errorf("expected %d events but got %d", 3, len(result.Events))
+	testCases := []testCase{
+		{
+			Filters: []query.Filter{
+				{
+					Column:    "source_port",
+					Condition: "=",
+					Value:     443.0,
+				},
+			},
+			ExpectedMatches: 3,
+		},
+		{
+			Filters: []query.Filter{
+				{
+					Column:    "source_port",
+					Condition: "!=",
+					Value:     443.0,
+				},
+			},
+			ExpectedMatches: 4,
+		},
+		{
+			Filters: []query.Filter{
+				{
+					Column:    "source_port",
+					Condition: "<",
+					Value:     443.0,
+				},
+			},
+			ExpectedMatches: 1,
+		},
+		{
+			Filters: []query.Filter{
+				{
+					Column:    "source_port",
+					Condition: "<=",
+					Value:     443.0,
+				},
+			},
+			ExpectedMatches: 4,
+		},
+		{
+			Filters: []query.Filter{
+				{
+					Column:    "source_port",
+					Condition: ">",
+					Value:     443.0,
+				},
+			},
+			ExpectedMatches: 3,
+		},
+		{
+			Filters: []query.Filter{
+				{
+					Column:    "source_port",
+					Condition: ">=",
+					Value:     443.0,
+				},
+			},
+			ExpectedMatches: 6,
+		},
+		{
+			Filters: []query.Filter{
+				{
+					Column:    "dest_address",
+					Condition: "matches",
+					Value:     "^52.54.+",
+				},
+			},
+			ExpectedMatches: 3,
+		},
+	}
+
+	for i, tc := range testCases {
+		result, err := ec.Query(query.Desc{
+			Filters: tc.Filters,
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(result.Events) != tc.ExpectedMatches {
+			t.Errorf("expected %d events but got %d for case %d",
+				tc.ExpectedMatches, len(result.Events), i)
+		}
 	}
 }
